7c5fb959ad74dfab3994d21b87816c284165a882
Mladja89 <goamladja2006@gmail.com>
2017-01-08 19:19:21 +0100

    Added Main Content

--- js/main.js
+++ js/main.js
@@ -1,5 +1,17 @@
 var app = angular.module("JuegoWebsite", []);
 
 app.controller('JuegoController', function($scope, $rootScope) {
-
+	$scope.arizona = [
+		{leagueName:'Arizona', age:25, abrevation:'AZ'},
+    	{name:'John', age:25, gender:'boy'},
+    	{name:'Jessie', age:30, gender:'girl'},
+    	{name:'Johanna', age:28, gender:'girl'},
+    	{name:'Joy', age:15, gender:'girl'},
+    	{name:'Mary', age:28, gender:'girl'},
+    	{name:'Peter', age:95, gender:'boy'},
+    	{name:'Sebastian', age:50, gender:'boy'},
+    	{name:'Erika', age:27, gender:'girl'},
+    	{name:'Patrick', age:40, gender:'boy'},
+    	{name:'Samantha', age:60, gender:'girl'}
+    ];
 });
